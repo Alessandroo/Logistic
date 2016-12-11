b48553e8c338bf9b85db3c348ce4e46ea0aebb84
DESKTOP-EU7S98I\Vojts <vojtseh2014@gmail.com>
2016-12-11 18:56:33 +0300

    no message

--- src/main/java/com/logistic/model/systemunits/entities/TimeTable.java
+++ src/main/java/com/logistic/model/systemunits/entities/TimeTable.java
@@ -1,28 +1,29 @@
 package com.logistic.model.systemunits.entities;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 /**
  * Created by Vojts on 10.12.2016.
  */
 public class TimeTable extends Entity {
 
-    private Date timeBegin;
-    private Date timeEnd;
+    private Timestamp timeBegin;
+    private Timestamp timeEnd;
 
-    public void setTimeBegin(Date timeBegin) {
+    public void setTimeBegin(Timestamp timeBegin) {
         this.timeBegin = timeBegin;
     }
 
-    public void setTimeEnd(Date timeEnd) {
+    public void setTimeEnd(Timestamp timeEnd) {
         this.timeEnd = timeEnd;
     }
 
-    public Date getTimeBegin() {
+    public Timestamp getTimeBegin() {
         return timeBegin;
     }
 
-    public Date getTimeEnd() {
+    public Timestamp getTimeEnd() {
         return timeEnd;
     }
 }
